62d848c8e9e1e0c20632136ed11cdb13ce39382e
Matheus Roxo Brandão <matheus.roxo.brandao31@gmail.com>
2020-12-23 01:04:02 -0300

    Finalizado

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using AulaPOO_Static.classes;
+
+namespace AulaPOO_Static
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Conversor.DolarParaReal(4f));
+            Console.WriteLine(Conversor.RealParaDolar(50f));
+
+            Console.WriteLine(Math.Pow(2,4));
+
+            Console.WriteLine(Conversor.EuroParaReal(4f));
+            Console.WriteLine(Conversor.RealParaEuro(50f));
+
+            Pedido p = new Pedido();
+            p.Testar();
+        }
+    }
+}
---
+++ classes/Pedido.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace AulaPOO_Static.classes
+{
+    public class Pedido
+    {
+        public void Testar(){
+            Console.WriteLine();
+
+            Console.WriteLine();
+        }
+    }
+}
